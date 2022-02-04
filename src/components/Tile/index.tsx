49f2cc3cede7bb7c1ad68ced3ede694f72d053b2
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2022-02-03 21:52:03 -0500

    add: animations

--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -6,10 +6,18 @@ type TileProps = {
 };
 
 export default function Tile({ content, state = "present" }: TileProps) {
+  const tileIsEmpty = content.trim().length === 0;
   return (
     <div
-      className="border border-slate-400 mx-auto flex items-center justify-center w-full font-bold text-center text-slate-50 before:pb-full"
-      style={{ fontSize: "2rem", lineHeight: "2rem" }}
+      className={`border ${
+        tileIsEmpty ? "border-slate-400" : "border-slate-200"
+      } mx-auto flex items-center justify-center w-full font-bold text-center text-slate-50 before:pb-full`}
+      style={{
+        fontSize: "2rem",
+        lineHeight: "2rem",
+        animationName: tileIsEmpty ? "" : "PopIn",
+        animationDuration: "100ms",
+      }}
     >
       {content.toUpperCase()}
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./pages/home";
 import "./styles/index.css";
+import "./styles/animations.css";
 
 ReactDOM.render(
   <React.StrictMode>
